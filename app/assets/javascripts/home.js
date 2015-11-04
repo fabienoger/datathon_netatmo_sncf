5de4b4d2387e338867d66657526115396f976475
fabienoger <fab-oger@live.fr>
2015-11-04 11:28:35 +0100

    get all stations and puts data in html + add markers with JS

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,17 @@
+// ##### Get data-stations #####
+$map = document.getElementById("map");
+stations = JSON.parse($map.getAttribute("data-stations"));
+console.log(stations);
+
+// ##### Initialize Map #####
 var map = L.map('map').setView([48.8, 2.34], 10);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'OpenStreetMap',
     maxZoom: 18,
 }).addTo(map);
+
+// ##### Add Markers #####
+for(i = 0; i < stations.length; i++)
+{
+  var marker = L.marker([stations[i].st_y, stations[i].st_x]).addTo(map);
+}
---
+++ app/models/station.rb
@@ -0,0 +1,3 @@
+class Station < ActiveRecord::Base
+  self.table_name = "station"
+end
